ed8b09283dd3d5573481cb53f5ca9d62d287cb72
Ian Piddington <ian@ianpiddington.net>
2019-04-03 16:46:03 +1300

    default load

---
+++ Vault Updater/Vault Updater/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace Vault_Updater
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.hostTextBox = new System.Windows.Forms.TextBox();
+            this.hostLabel = new System.Windows.Forms.Label();
+            this.connectButton = new System.Windows.Forms.Button();
+            this.label14 = new System.Windows.Forms.Label();
+            this.vaultComboBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnGetProperties = new System.Windows.Forms.Button();
+            this.propertyComboBox = new System.Windows.Forms.ComboBox();
+            this.btnAddToClass = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // hostTextBox
+            // 
+            this.hostTextBox.Location = new System.Drawing.Point(190, 20);
+            this.hostTextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.hostTextBox.Name = "hostTextBox";
+            this.hostTextBox.Size = new System.Drawing.Size(268, 20);
+            this.hostTextBox.TabIndex = 32;
+            this.hostTextBox.Text = "localhost";
+            // 
+            // hostLabel
+            // 
+            this.hostLabel.AutoSize = true;
+            this.hostLabel.Location = new System.Drawing.Point(11, 22);
+            this.hostLabel.Name = "hostLabel";
+            this.hostLabel.Size = new System.Drawing.Size(64, 13);
+            this.hostLabel.TabIndex = 33;
+            this.hostLabel.Text = "Server host:";
+            // 
+            // connectButton
+            // 
+            this.connectButton.Location = new System.Drawing.Point(14, 48);
+            this.connectButton.Name = "connectButton";
+            this.connectButton.Size = new System.Drawing.Size(155, 23);
+            this.connectButton.TabIndex = 38;
+            this.connectButton.Text = "Connect";
+            this.connectButton.UseVisualStyleBackColor = true;
+            this.connectButton.Click += new System.EventHandler(this.connectButton_Click);
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(187, 52);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(473, 13);
+            this.label14.TabIndex = 37;
+            this.label14.Text = "(Connection defaults: TCP/IP, port 2266, current Windows user. Modify app soure c" +
+    "ode if needed.)";
+            // 
+            // vaultComboBox
+            // 
+            this.vaultComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.vaultComboBox.Enabled = false;
+            this.vaultComboBox.FormattingEnabled = true;
+            this.vaultComboBox.Location = new System.Drawing.Point(190, 97);
+            this.vaultComboBox.Name = "vaultComboBox";
+            this.vaultComboBox.Size = new System.Drawing.Size(268, 21);
+            this.vaultComboBox.TabIndex = 36;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 99);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 35;
+            this.label1.Text = "Target vault:";
+            // 
+            // btnGetProperties
+            // 
+            this.btnGetProperties.Location = new System.Drawing.Point(14, 134);
+            this.btnGetProperties.Name = "btnGetProperties";
+            this.btnGetProperties.Size = new System.Drawing.Size(155, 23);
+            this.btnGetProperties.TabIndex = 39;
+            this.btnGetProperties.Text = "Get properties";
+            this.btnGetProperties.UseVisualStyleBackColor = true;
+            this.btnGetProperties.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // propertyComboBox
+            // 
+            this.propertyComboBox.FormattingEnabled = true;
+            this.propertyComboBox.Location = new System.Drawing.Point(190, 178);
+            this.propertyComboBox.Name = "propertyComboBox";
+            this.propertyComboBox.Size = new System.Drawing.Size(268, 21);
+            this.propertyComboBox.TabIndex = 40;
+            
+            // 
+            // btnAddToClass
+            // 
+            this.btnAddToClass.Location = new System.Drawing.Point(14, 208);
+            this.btnAddToClass.Name = "btnAddToClass";
+            this.btnAddToClass.Size = new System.Drawing.Size(155, 23);
+            this.btnAddToClass.TabIndex = 41;
+            this.btnAddToClass.Text = "Add to Doc Classes";
+            this.btnAddToClass.UseVisualStyleBackColor = true;
+            this.btnAddToClass.Click += new System.EventHandler(this.btnAddToClass_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1009, 450);
+            this.Controls.Add(this.btnAddToClass);
+            this.Controls.Add(this.propertyComboBox);
+            this.Controls.Add(this.btnGetProperties);
+            this.Controls.Add(this.connectButton);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.vaultComboBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.hostTextBox);
+            this.Controls.Add(this.hostLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox hostTextBox;
+        private System.Windows.Forms.Label hostLabel;
+        private System.Windows.Forms.Button connectButton;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.ComboBox vaultComboBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnGetProperties;
+        private System.Windows.Forms.ComboBox propertyComboBox;
+        private System.Windows.Forms.Button btnAddToClass;
+    }
+}
+
---
+++ Vault Updater/Vault Updater/Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MFilesAPI;
+
+namespace Vault_Updater
+{
+    public partial class Form1 : Form
+    {
+        #region Class members
+        // M-Files API objects used for the connection.
+        private Vault vault = null;
+        private MFilesServerApplication app;
+        private MFServerConnection conn;
+
+
+        // A private class for the items put into the Vault selection ComboBox.
+        private class VaultComboBoxItem
+        {
+            public string VaultName;
+            public string VaultGUID;
+
+            public VaultComboBoxItem(string name, string guid)
+            {
+                VaultName = name;
+                VaultGUID = guid;
+
+            }
+            public override string ToString()
+            {
+                // Generates the text shown in the combo box
+                return VaultName + " " + VaultGUID;
+            }
+        }
+
+        private class PropertyComboBoxItem
+        {
+            public string PropName;
+            public int propID;
+
+            public PropertyComboBoxItem(string name, int id)
+            {
+                PropName = name;
+                propID = id;
+            }
+            public override string ToString()
+            {
+                return PropName + " " + propID;
+            }
+
+
+        }
+
+        #endregion
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void connectButton_Click(object sender, EventArgs e)
+        {
+            // Using the Server mode to connect to the M-Files Server.
+            app = new MFilesServerApplication();
+
+            string host = hostTextBox.Text;
+
+            // Using the default values for connecting to server (HTTP, 2266, current Windows user, etc.), 
+            // for details and options see:
+            // https://www.m-files.com/api/documentation/latest/index.html#MFilesAPI~MFilesServerApplication.html
+            conn = app.Connect(NetworkAddress: host);
+
+            // Populate the combo box so that the user can choose which vault to use.
+            VaultsOnServer vaults = app.GetOnlineVaults();
+            foreach (VaultOnServer v in vaults)
+            {
+                vaultComboBox.Items.Add(new VaultComboBoxItem(v.Name, v.GUID));
+            }
+            vaultComboBox.Enabled = true;
+            //       EnableButtons(true);
+
+            // Must re-start the tool if need to connect to another server.
+            hostTextBox.Enabled = false;
+            connectButton.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Connect to the vault and continue if successful.
+            if (!ConnectToSelectedVault()) return;
+
+            PropertyDefsAdmin propDefs = vault.PropertyDefOperations.GetPropertyDefsAdmin();
+
+            foreach (PropertyDefAdmin pda in propDefs)
+            {
+                propertyComboBox.Items.Add(new PropertyComboBoxItem(pda.PropertyDef.Name, pda.PropertyDef.ID));
+            }
+
+
+        }
+
+        private bool ConnectToSelectedVault()
+        {
+            if (vaultComboBox.SelectedItem == null)
+            {
+                MessageBox.Show("Please select the target vault.");
+                return false;
+            }
+
+            string currentlySelectedVaultGUID = ((VaultComboBoxItem)vaultComboBox.SelectedItem).VaultGUID;
+
+            // No need to re-connect if already connected to the selected vault.
+            if (vault == null || !vault.GetGUID().Equals(currentlySelectedVaultGUID))
+            {
+                vault = app.LogInToVault(currentlySelectedVaultGUID);
+                Console.WriteLine("Connected to: " + vault.Name);
+            }
+
+            vaultComboBox.Enabled = false;  // Must re-start the app if the vault needs to be changed.
+          //  EnableButtons(false);
+
+            return true;
+        }
+
+        private void btnAddToClass_Click(object sender, EventArgs e)
+        {
+            // Connect to the vault and continue if successful.
+            if (!ConnectToSelectedVault()) return;
+
+            ObjectClasses classes = vault.ClassOperations.GetAllObjectClasses();
+            foreach (ObjectClass cl in classes)
+            {
+                if (cl.ObjectType == 0)
+                {
+                    ObjectClassAdmin cla = vault.ClassOperations.GetObjectClassAdmin(cl.ID);
+                    // Need to get the AssociatedPropertyDefs from the underlying ObjectClass (except
+                    // for a few built-in properties), otherwise UpdateObjectClassAdmin() will fail.
+                    CopyPropertiesToObjectClassAdmin(cla, cl);
+                    AssociatedPropertyDef apd = new AssociatedPropertyDef();
+                    apd.PropertyDef = ((PropertyComboBoxItem)propertyComboBox.SelectedItem).propID;
+                    cla.AssociatedPropertyDefs.Add(-1, apd);
+                    vault.ClassOperations.UpdateObjectClassAdmin(cla);
+
+                    
+                }
+            }
+            MessageBox.Show("Done");
+
+        }
+        private void CopyPropertiesToObjectClassAdmin(ObjectClassAdmin oca, ObjectClass oc)
+        {
+            oca.AssociatedPropertyDefs = new AssociatedPropertyDefs();
+
+            // Skip certain built-in properties present in the original ObjectClass object, these cause problems with
+            // UpdateObjectClassAdmin() later.
+            foreach (AssociatedPropertyDef def in oc.AssociatedPropertyDefs)
+            {
+                if ((def.PropertyDef >= 20 && def.PropertyDef <= 25) ||
+                    (def.PropertyDef >= 30 && def.PropertyDef <= 32) ||
+                    def.PropertyDef == 89 ||
+                    def.PropertyDef == 101)
+                {
+                    continue;
+                }
+                else
+                {
+                    //Console.WriteLine("Adding: " + oc.Name + ", " + def.PropertyDef);
+                    oca.AssociatedPropertyDefs.Add(-1, def);
+                }
+            }
+        }
+    }
+}
