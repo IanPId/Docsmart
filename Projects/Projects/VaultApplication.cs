a72955d8185b41424cadf533ce7980e73321bdb3
Ian Piddington <ian@ianpiddington.net>
2019-04-03 16:33:08 +1300

    Default Project Checkin

---
+++ Projects/Projects/VaultApplication.cs
@@ -0,0 +1,13 @@
+﻿using MFiles.VAF;
+
+namespace Projects
+{
+    /// <summary>
+    /// The entry point for this Vault Application Framework application.
+    /// </summary>
+    /// <remarks>Examples and further information available on the developer portal: http://developer.m-files.com/. </remarks>
+    public class VaultApplication
+        : VaultApplicationBase
+    {
+    }
+}
